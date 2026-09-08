ab0232d669dd33d71c610917a66b09b8b30ace29
jamiehaick-ai <jamiehaick5@gmail.com>
2026-09-08 15:21:24 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bumping CACHE_NAME forces old cached files to be replaced on next load.
-const CACHE_NAME = "livelogger-lite-v8";
+const CACHE_NAME = "livelogger-lite-v9";
 const SHELL_FILES = [
   "./",
   "./index.html",
